b368b7a086e31b15fe523d8ab361dd0ee109fdff
DavidF <forbesd@btopenworld.com>
2021-04-13 16:18:13 +0100

    Quotes array

--- a_random_quote_generator-v1/js/script.js
+++ a_random_quote_generator-v1/js/script.js
@@ -11,11 +11,42 @@ project 1 - A Random Quote Generator
  * `quotes` array 
 ***/
 
+let quotes = [ 
+
+  {
+    quote:  ' "The best way to get started is to quite talking and begin doing." ',
+    source: 'Walt Disney',
+    citation: 'The Disney Company'
+}
+
+{
+  quote:  ' "The pessimist sees difficulty in every opportunity. The optimist sees opportunity in every difficulty." ',
+  source: 'Winston Churchill',
+  year: '1945'
+}
+
+{
+  quote:  ' "Do not let yesterday take up too much of today. " ',
+  source: 'Will Rogers'
+}
+
+{
+  quote:  ' "It is not whether you get knocked down, it is whether you get up. " ',
+  source: 'Vince Lombardi',
+}
+
+{
+  quote:  ' "If you are working on something that you really care about, you do not have to be pushed. The vision pulls you." ',
+  source: 'Steve Jobs'
+}
+
+];
 
 
 /***
  * `getRandomQuote` function
 ***/
+
 
 
 
